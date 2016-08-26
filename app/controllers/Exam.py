a9d3ae5436938145abeb97e9f93e0594fe13dfbb
Ricky Ratcliff <ricky0ratcliff@gmail.com>
2016-08-26 11:49:05 -0700

    first commit

---
+++ app/controllers/Exam.py
@@ -0,0 +1,63 @@
+
+from __future__ import print_function
+from system.core.controller import *
+import sys #sys.stderr.write('text_you_want_to_print\n')
+class Exam(Controller):
+    def __init__(self, action):
+        super(Exam, self).__init__(action)
+    
+        self.load_model('ExamModel')
+        self.db = self._app.db
+
+
+   
+    def index(self):
+        return self.load_view('index.html')
+
+    def create(self):
+        user_info = {
+            "name" : request.form['name'],
+            "alias" : request.form['alias'],
+            "email" : request.form['email'],
+            "password" : request.form['password'],
+            "dob" : request.form['dob']
+        }
+
+        print(user_info, file=sys.stderr)
+        user_id = self.models['ExamModel'].add_user(user_info)
+
+        session['id'] = user_id[0]['id']
+        session['name'] = user_id[0]['name']
+        session['alias'] = user_id[0]['alias']
+        # session['email'] = user_id[0]['email']
+        # session['last_name'] = user_id[0]['last_name']
+
+        users = self.models['ExamModel'].get_users(user_id)
+
+        sys.stderr.write('Test2\n')
+        return self.load_view('pokes.html', users=users)
+
+    def login(self):
+        user_info = {
+            "email" : request.form['email'],
+            "password" : request.form['password']
+        }
+        sys.stderr.write('Test3\n')
+        user_id = self.models['ExamModel'].login_user(user_info)
+
+        session['id'] = user_id[0]['id']
+        session['name'] = user_id[0]['name']
+        session['alias'] = user_id[0]['alias']
+        # session['email'] = user_id[0]['email']
+        # session['last_name'] = user_id[0]['last_name']
+        sys.stderr.write('Test4\n')
+
+        users = self.models['ExamModel'].get_users(user_id)
+        sys.stderr.write('Test5\n')
+        return self.load_view('pokes.html', users=users)
+    # def pokes(self):
+    #     return self.load_view('pokes.html')
+
+    def logout(self):
+        session.clear()
+        return self.load_view('index.html')
---
+++ app/models/ExamModel.py
@@ -0,0 +1,31 @@
+
+from __future__ import print_function
+from system.core.model import Model
+import sys #sys.stderr.write('text_you_want_to_print\n')
+class ExamModel(Model):
+    def __init__(self):
+        super(ExamModel, self).__init__()
+   
+    def add_user(self, user):
+        query = "INSERT INTO users (name, alias, email, password, dob, created_at) VALUES (:name, :alias, :email, :password, :dob, NOW())"
+        sys.stderr.write('Test6\n')
+
+        self.db.query_db(query, user)
+
+        query = 'SELECT * FROM users WHERE users.email = :email AND users.password = :password'
+        sys.stderr.write('Test7\n')
+        return self.db.query_db(query, user)
+
+
+    def get_users(self, user):
+        query = 'SELECT * FROM users WHERE id = id'
+        data = {'id' : id}
+        return self.db.query_db(query)
+
+
+
+    def login_user(self, user):
+
+        query ='SELECT * FROM users WHERE users.email = :email AND users.password = :password'
+        return self.db.query_db(query, user)
+
